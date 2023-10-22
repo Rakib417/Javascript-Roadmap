9ca4c32882f6e619e0b2bbc9ddb36ba0c6cd1950
rakib417 <rakib417@gmail.com>
2023-10-22 12:29:08 +0600

    add

---
+++ Les 2 - First Js Project/library.js
@@ -0,0 +1,50 @@
+        // var text = prompt("Andwer Your name : ");
+        // var len = text.length;
+        // document.write("Number of character " + len)
+
+
+
+        // var info = prompt("Type your country name = ");
+        // var mainText = info.length;
+        // document.write("Info character count= " + mainText);
+
+
+        // var text = "bangladesh";        
+        // document.write( text.charAt(2));
+
+        // var text = "banGladesh";
+        // // var txt = text.toUpperCase();
+        // var txt = text.toLowerCase();
+        // document.write(txt);
+
+        // var text1 = "banGladesh";
+        // var text2 = "Is a Country";        
+        // var tx= text1.concat(text2);
+        // var txUp = tx.toUpperCase();
+        // var txCnt = txUp.length;
+        // var txInfo = txUp + " full line have total alfabet = " + txCnt;
+
+        // document.write(txInfo);
+
+
+        // var text1 = "Bangladesh";
+        // tx = text1.slice(3,6);
+
+        // document.write(tx);
+
+
+        // Task 2 
+        var firstName = "Rakib";
+        var lastName = " Ullah";
+        var fullName = firstName.concat(lastName);
+        var upFullName = fullName.toUpperCase();
+        var lnt = upFullName.length;
+        var posWord = fullName.charAt(2);
+        var slWord = fullName.slice(1,9)
+        var mainRst = "My Full name is = " + upFullName + "<br>" + "Total alphabet Count = " + lnt + "<br>" + "2nd Position from Full name is = " + posWord + "<br>" + "Slice Word = " + slWord;
+
+        document.write(mainRst);
+
+        
+
+
