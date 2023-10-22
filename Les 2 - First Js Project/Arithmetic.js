006cc3b3a02696854294cb92aae495144f6aaf32
rakib417 <rakib417@gmail.com>
2023-10-22 14:18:13 +0600

    add

---
+++ Les 2 - First Js Project/Arithmetic.js
@@ -0,0 +1,33 @@
+// Arithmetic Operator ->    +,-,*,/,%(modulus) , **(Exponent), ++, --
+
+// console.log(5+7)
+// console.log(5-7)
+// console.log(5*7)
+// console.log(5/7)
+// console.log(5%7)
+// console.log(2**3)
+// var a = 6;
+// console.log(a--)
+// console.log(--a)
+// console.log(--a)
+// console.log(a--)
+// var b = 9;
+// console.log(++b)
+// console.log(b++)
+// console.log(b)
+
+
+
+//Asssignment Operator -> =, += ,-= ,*= , /=, %=, **=
+var x = 3;
+x +=6; // x = x + 6
+
+console.log(x);
+
+
+var x = 4;
+x *=4;
+console.log(x);
+
+x /= 3;
+console.log(x);
