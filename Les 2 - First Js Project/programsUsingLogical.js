0be222c697bb22254aef0ed10936d3b47a1c0972
Md. Rakib Ullah <rakib417@gmail.com>
2023-10-24 16:45:31 +0600

    add

---
+++ Les 2 - First Js Project/programsUsingLogical.js
@@ -0,0 +1,47 @@
+// var marks = prompt("Enter Your Marks :");
+
+// if (marks > 100 ||  marks < 0)
+//     console.log("INVALID");
+// else if (marks >= 80 && marks <= 100) 
+//     console.log("A+");
+// else if (marks >= 70 && marks <= 79) 
+//     console.log("A");
+// else if (marks >= 60 && marks <= 69) 
+//     console.log("-A");
+// else if (marks >= 50 && marks <= 59) 
+//     console.log("B");
+// else if (marks >= 40 && marks <= 49) 
+//     console.log("C");
+// else if (marks >= 33 && marks <= 39) 
+//     console.log("D");
+// else console.log("Fail");
+
+
+// var num1 = prompt("Enter Your num1 :");
+// var num2 = prompt("Enter Your num2 :");
+// var num3 = prompt("Enter Your nub3 :");
+
+
+// if(num1> num2 && num1 > num3)
+//     console.log("Large NUmber = " + num1);
+
+// else if(num2> num1 && num2 > num3)
+//     console.log("Large NUmber = " + num2);
+
+// else
+//     console.log("Large NUmber = " + num3);
+
+
+
+
+// Vowel / Consonant
+
+var lett = prompt("Enter a Latter :");
+
+letter = lett.toLowerCase();
+
+if ( letter == "a" || letter == "e" || letter == "i" || letter == "o" || letter == "u")
+    console.log ( " Its Vowel ");
+else 
+    console.log("Consonant");
+
