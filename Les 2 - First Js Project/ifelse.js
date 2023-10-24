2a30b64c6c26cb9341c829770c425989e15156d7
Md. Rakib Ullah <rakib417@gmail.com>
2023-10-24 16:26:48 +0600

    add

--- Les 2 - First Js Project/ifelse.js
+++ Les 2 - First Js Project/ifelse.js
@@ -1,1 +1,30 @@
- we
+// var number = 5;
+
+// if(number%2 == 0)
+// console.log("Even");
+
+// else
+// console.log("Odd");
+
+// var num = prompt("enter a number = ");
+
+// if (num > 0 )
+// console.log("positive");
+
+// else if (num < 0 )
+// console.log("Nagetive");
+
+// else
+// console.log("Zero");
+
+// task 4
+
+var grd = prompt("Enter Your SSC Marks Here : ");
+
+if (grd >= 80) document.write("Your SSC Grade Is A+");
+else if (grd >= 70) document.write("Your SSC Grade Is A");
+else if (grd >= 60) document.write("Your SSC Grade Is -A");
+else if (grd >= 50) document.write("Your SSC Grade Is B");
+else if (grd >= 40) document.write("Your SSC Grade Is C");
+else if (grd >= 33) document.write("Your SSC Grade Is D");
+else document.write("We are Very Sorry You Fail");
