5b678861ab16420c0998802f79ac717456ee6f1d
rakib417 <rakib417@gmail.com>
2023-10-26 17:20:49 +0600

    new

---
+++ Les 2 - First Js Project/ternaryoperator.js
@@ -0,0 +1,33 @@
+// var num = Number(prompt("Enter a Number "));
+// if(num>0){
+//     console.log("possitive");
+// }
+
+// else{
+//     console.log("Negative");
+// }
+
+
+// var result = num > 0? "Positive" : "Negetive";
+
+// document.write(result);
+
+
+
+// if(num>0){
+//     console.log("possitive");
+// }
+
+// else if(num < 0){
+//     console.log("Negative");
+// }
+
+// else{
+//     console.log("Zero");
+// }
+
+
+var num = Number(prompt("Enter a Number "));
+var res = num > 0 ? "possitive" : num < 0 ? " negative": "zero"
+
+console.log(res);
