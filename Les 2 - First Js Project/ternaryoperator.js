aae582df0051179431e9a9ac0e24b04bea09ef9a
Md. Rakib Ullah <rakib417@gmail.com>
2023-10-28 10:58:11 +0600

    add

--- Les 2 - First Js Project/ternaryoperator.js
+++ Les 2 - First Js Project/ternaryoperator.js
@@ -27,7 +27,27 @@
 // }
 
 
-var num = Number(prompt("Enter a Number "));
-var res = num > 0 ? "possitive" : num < 0 ? " negative": "zero"
+// var num = Number(prompt("Enter a Number "));
+// var res = num > 0 ? "possitive" : num < 0 ? " negative": "zero"
 
-console.log(res);
+// console.log(res);
+
+
+
+
+// var number = Number(prompt("Enter Number : "));
+// var rs = number > 0 ?"positive" : number < 0 ? "Negetv" : "Zero";
+// console.log(rs);
+
+
+
+
+
+// Task Ternary Operator
+var num1 = prompt("ENter 1st Number");
+var num2 = prompt("ENter 2nd Number");
+var num3 = prompt("Enter 3rd Number"); 
+
+
+var rsts= num1 > num2 && num1 > num3 ? num1 : num2 > num1 && num2 > num3 ? num2 : num3 ;
+document.write(rsts);
