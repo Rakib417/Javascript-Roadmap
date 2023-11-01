4ca6f91af2c286c0b6276075e600d20ff533a587
Md. Rakib Ullah <rakib417@gmail.com>
2023-11-01 23:33:50 +0600

    add

--- Les 2 - First Js Project/array.js
+++ Les 2 - First Js Project/array.js
@@ -60,19 +60,28 @@
 
 
 
-var num = new Array();
-for(var i = 0 ; i<5;  i++ ){
-    num[i] = parseInt(prompt("enter Number"));
-}
+// var num = new Array();
+// for(var i = 0 ; i<5;  i++ ){
+//     num[i] = parseInt(prompt("enter Number"));
+// }
 
+// var sum = 0;
+
+
+
+// for(var i = 0;i<5;i++){
+//     console.log(num[i]);
+//     sum = sum + num[i];
+// }
+
+// console.log("Sum = " + sum);
+
+
+var num = [10,20,30,40,50];
 var sum = 0;
 
-
-
-for(var i = 0;i<5;i++){
+for(var i = 0; i< 5; i++){
     console.log(num[i]);
-    sum = sum + num[i];
+    
 }
-
-console.log("Sum = " + sum);
 
