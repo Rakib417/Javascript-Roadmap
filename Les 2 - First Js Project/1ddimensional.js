14f541042883e4d30f8506cee02ea11829cd69dd
rakib417 <rakib417@gmail.com>
2023-11-20 18:33:25 +0600

    add

--- Les 2 - First Js Project/1ddimensional.js
+++ Les 2 - First Js Project/1ddimensional.js
@@ -1,13 +1,33 @@
-var names = ["Asraf", "Sakib", "Mushfiq", "Rakib", "zakir", "Sakib"];
+// var names = ["Asraf", "Sakib", "Mushfiq", "Rakib", "zakir", "Sakib"];
 
-for(var i=0; i<6; i++){
-    console.log(names[i])
+// for(var i=0; i<6; i++){
+//     console.log(names[i])
+// }
+
+
+// task 9.
+
+// One Dmensional Array
+// Create a Function call HighstScore That
+// Receive a 1D Array Called scores
+// Return the Highest Score
+
+
+
+function highstScore(scores){
+    let max = scores[0];
+    for(var i = 1 ; i < scores.length; i++){
+        if(max<scores[i]){
+            max = scores[i];
+        }
+    }
+    return max;
 }
 
 
+var scores = [21,22,34,54,64];
 
-var scor = 
+var maxscor = highstScore(scores);
 
-function highScor(scor){
 
-}
+console.log(maxscor);
