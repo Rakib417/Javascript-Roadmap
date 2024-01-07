b670406f31195c6e031272f87978a11dedfa0141
Md. Rakib Ullah <rakib417@gmail.com>
2024-01-07 18:51:14 +0600

    add

---
+++ Les 2 - First Js Project/2ddimensional.js
@@ -0,0 +1,9 @@
+// 2D Array
+// Create a function called highestRunScorer
+// Receive a 2d array called playersInfo
+// Based on highest ScreenOrientation, return the name of the player
+// Higest Run Scorer
+
+
+
+
