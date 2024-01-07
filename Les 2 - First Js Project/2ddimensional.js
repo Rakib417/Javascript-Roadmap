4a2e37164888c3b02a181f031e094659ad2adb96
Md. Rakib Ullah <rakib417@gmail.com>
2024-01-08 00:21:51 +0600

    add

--- Les 2 - First Js Project/2ddimensional.js
+++ Les 2 - First Js Project/2ddimensional.js
@@ -4,6 +4,65 @@
 // Based on highest ScreenOrientation, return the name of the player
 // Higest Run Scorer
 
+function highestRunScorer(playersInfo){
+
+    var highestScorer = playersInfo[0][0];
+    var highestScore = playersInfo[0][1];
+
+
+    for (var x=0; x<playersInfo.length; x++){
+        if(highestScore<playersInfo[x][1]){
+            highestScore =playersInfo [x][1];
+            highestScorer = playersInfo[x][0];
+        }
+    }
+    return highestScorer
+}
+
+var playersInfo = [
+    ["Ashraful", 95],
+    ["Rakib", 98],
+    ["Sakib", 188],
+    ["nks", 66],
+]
+
+var names = highestRunScorer(playersInfo);
+console.log(names);
 
 
 
+
+
+
+
+
+
+
+function highRunScore(score) { 
+
+    var highScores = score[0][0];
+    var highScore = score [0][1];
+
+    for(var i=0; i<score.length;i++){
+        if(highScore<score[i][1]){
+            highScores = score[i][0];
+            highScore = score[i][1];
+            
+        }
+    }
+    return highScores;
+ }
+
+
+
+var score = [
+    ["Ashraful", 255],
+    ["Rakib", 198],
+    ["Sakib", 208],
+    ["nks", 166],
+    ["hks", 266],
+    ["€Kks", 366],
+    ["swks", 166],
+]
+var nms = highRunScore(score);
+console.log(nms);
--- Task only copy/taskonly.js
+++ Task only copy/taskonly.js
@@ -135,3 +135,34 @@ var maxscr = highscor(score);
 
 console.log(maxscr);
 
+
+
+// Task 10
+function highRunScore(score) { 
+
+    var highScores = score[0][0];
+    var highScore = score [0][1];
+
+    for(var i=0; i<score.length;i++){
+        if(highScore<score[i][1]){
+            highScores = score[i][0];
+            highScore = score[i][1];
+            
+        }
+    }
+    return highScores;
+ }
+
+
+
+var score = [
+    ["Ashraful", 255],
+    ["Rakib", 198],
+    ["Sakib", 208],
+    ["nks", 166],
+    ["hks", 266],
+    ["€Kks", 366],
+    ["swks", 166],
+]
+var nms = highRunScore(score);
+console.log(nms);
