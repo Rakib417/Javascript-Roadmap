239788d9455c19da14b305f15f41fb32594846e9
rakib417 <rakib417@gmail.com>
2024-01-10 14:52:42 +0600

    add

--- Les 2 - First Js Project/GuessingGame.js
+++ Les 2 - First Js Project/GuessingGame.js
@@ -6,25 +6,48 @@
 
 
 
-var numbOfWin = 0;
-var numbOfLost = 0;
+// var numbOfWin = 0;
+// var numbOfLost = 0;
 
 
-for(var x = 1; x<=5; x++){
-    var guessNumb = parseInt(prompt("Enter a number From 1 to 5 : "));
+// for(var x = 1; x<=5; x++){
+//     var guessNumb = parseInt(prompt("Enter a number From 1 to 5 : "));
 
-    var randomNumb = Math.floor(Math.random()*5) + 1;
-    if (guessNumb==randomNumb){
-        console.log("You have Won");
-        numbOfWin++;
-    }else{
-        console.log("You have Lost. Random number was " + randomNumb );
-        numbOfLost++;
-    }
+//     var randomNumb = Math.floor(Math.random()*5) + 1;
+//     if (guessNumb==randomNumb){
+//         console.log("You have Won");
+//         numbOfWin++;
+//     }else{
+//         console.log("You have Lost. Random number was " + randomNumb );
+//         numbOfLost++;
+//     }
     
 
+// }
+
+// document.write("Total Number of Won : " + numbOfWin + "<br>");
+// document.write("Total Number of Lost : " + numbOfLost);
+
+
+
+
+var winnumber = 0;
+var lossnumber = 0;
+
+for(var i=1; i<=5; i++){
+    var gusnumber = parseInt(prompt("Enter number from 1 to 5"));
+    var randomnumber = Math.floor(Math.random()*6)+1;
+
+
+    if(gusnumber==randomnumber){
+        console.log("Won Guess Number = " + randomnumber);
+        winnumber++;
+    }else{
+        console.log("Loss Guess Number = " + randomnumber);
+        lossnumber++;
+    }
 }
 
-document.write("Total Number of Won : " + numbOfWin + "<br>");
-document.write("Total Number of Lost : " + numbOfLost);
+document.write("Total Number win = " + winnumber + " <br>");
+document.write("Total loss Number = " + lossnumber);
 
