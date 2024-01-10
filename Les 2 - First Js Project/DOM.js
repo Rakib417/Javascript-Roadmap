ff2d7ee572b95a1ef2b3a1159e1a6de4964c09cb
rakib417 <rakib417@gmail.com>
2024-01-10 17:16:14 +0600

    add

---
+++ Les 2 - First Js Project/DOM.js
@@ -0,0 +1,11 @@
+// getelementById
+
+var myheading = document.getElementById("heading1");
+
+myheading.innerHTML = "Hello";
+
+
+document.getElementById("pera").innerHTML = "Hay";
+
+
+document.getElementById("heading2").innerHTML = "Hay";
