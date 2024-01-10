f49343d9684b701a332f1abae34f6c63c4ebe6ce
rakib417 <rakib417@gmail.com>
2024-01-10 15:38:17 +0600

    add

---
+++ Les 2 - First Js Project/Dateobjectanddatemethods.js
@@ -0,0 +1,22 @@
+var date = new Date();
+console.log(date);
+
+
+
+var year = date.getFullYear();
+console.log(year);
+
+var month = date.getMonth();
+console.log(month);
+
+var curdate = date.getDate();
+console.log(curdate);
+
+var curday = date.getDay();
+console.log(curday);
+
+var curhours = date.getHours();
+console.log(curhours);
+
+var curmin = date.getMinutes();
+console.log(curmin);
