bb731d1ec8675ae02d4acc40c094c59afbbc43bd
rakib417 <rakib417@gmail.com>
2024-01-14 16:02:18 +0600

    add

--- Les 2 - First Js Project/DOM.js
+++ Les 2 - First Js Project/DOM.js
@@ -5,7 +5,10 @@ var myheading = document.getElementById("heading1");
 myheading.innerHTML = "Hello";
 
 
-document.getElementById("pera").innerHTML = "Hay";
+document.getElementById("pera").innerHTML = "pera niyan na";
 
 
-document.getElementById("heading2").innerHTML = "Hay";
+
+var myheading2 = document.getElementById("heading2");
+
+myheading2.innerHTML = "Hello 2nd ";
