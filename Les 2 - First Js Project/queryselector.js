77ef4eeaa555641cb4dd501b3c7e3f802a6c19f1
rakib417 <rakib417@gmail.com>
2024-01-23 20:02:53 +0600

    add

---
+++ Les 2 - First Js Project/queryselector.js
@@ -0,0 +1,21 @@
+// getElementsById()
+// getElementsByTagName()
+// getElementsByClassName()
+// querySelectir()
+
+
+// document.getElementById("pid").innerHTML = "THis :LOL";
+
+// document.querySelector("#pid").innerHTML = "THis :NEED";
+
+
+// document.querySelector(".pclass").innerHTML = "THis LOL";
+
+
+// document.querySelector("p").innerHTML = "THis HAHAHA";
+
+
+
+
+
+document.querySelector(".my-div a").innerHTML = "New Info";
