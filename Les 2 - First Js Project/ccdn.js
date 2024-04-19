222955dd52067c179ff437dd060d2f0b5bad3dd7
unknown <rakib417@gmail.com>
2024-04-19 08:35:00 -0700

    add

---
+++ Les 2 - First Js Project/ccdn.js
@@ -0,0 +1,13 @@
+var myvar = document.querySelector("#peraid1");
+
+function addStyle() {
+
+    myvar.classList.add("para-style");
+
+}
+
+function removeStyle() {
+
+    myvar.classList.remove("para-style");
+
+}
