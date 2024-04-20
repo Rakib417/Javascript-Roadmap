0567d997961d84d4cc0647939b41ea1bf2df19d9
unknown <rakib417@gmail.com>
2024-04-19 23:16:07 -0700

    Add Event lister

--- Les 2 - First Js Project/eventlisterm.js
+++ Les 2 - First Js Project/eventlisterm.js
@@ -1,14 +1,26 @@
-var len = document.querySelectorAll(".mybutton").length;
+// var len = document.querySelectorAll(".mybutton").length;
 
-for (var i = 0; i < len; i++) {
+// for (var i = 0; i < len; i++) {
 
+//     document.querySelectorAll(".mybutton")[i].addEventListener("click", function() {
+
+//         var text = this.innerHTML;
+//         document.querySelector("h1").innerHTML = text + " is Clicked";
+
+//     });
+
+
+
+// }
+
+
+
+
+var lent = document.querySelectorAll(".mybutton").length;
+for (i = 0; i < lent; i++) {
     document.querySelectorAll(".mybutton")[i].addEventListener("click", function() {
 
         var text = this.innerHTML;
         document.querySelector("h1").innerHTML = text + " is Clicked";
-
     });
-
-
-
 }
