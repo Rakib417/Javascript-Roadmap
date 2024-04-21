8d1eefe3bd9e6996abbae3a14b965859d3828b7b
rakib417 <rakib417@gmail.com>
2024-04-21 16:43:44 +0600

    play audio and add animation

--- Les 2 - First Js Project/addremoveanimation.js
+++ Les 2 - First Js Project/addremoveanimation.js
@@ -2,15 +2,38 @@ for(var i = 0 ; i < 3; i++){
     document.querySelectorAll(".mybtn")[i].addEventListener("click", function(){
         var text = this.innerHTML;
         console.log(text);
+        audioplay(text);
+        playAnimation(text);
 
 
-        switch(text){
-            case "a":
-                var audio = new Audio('audio_file.mp3');
-                audio.play();
-
-        }
     });
 }
 
 
+function audioplay(text){
+    switch(text){
+        case "a":
+            var audio = new Audio('sounds/01.mp3');
+            audio.play();
+
+            case "b":
+            var audio = new Audio('sounds/02.mp3');
+            audio.play();
+
+            case "c":
+            var audio = new Audio('sounds/03.mp3');
+            audio.play();
+
+    }
+}
+
+
+
+function playAnimation(text){
+   var selectbtn = document.querySelector("." +  text);
+   selectbtn.classList.add("anim");
+
+   setTimeout(function(){
+    selectbtn.classList.remove("anim");
+   }, 1000);
+}
