5f7e166a537620585e88087a5eaab32b3cffbbb4
rakib417 <rakib417@gmail.com>
2024-04-22 17:48:59 +0600

    Video Event DOM event Object

--- Les 2 - First Js Project/eventobject.js
+++ Les 2 - First Js Project/eventobject.js
@@ -1,2 +1,31 @@
 // audio / video EventSource
 // canplay, play, playing , pause, progress, ende, valumechange, waiting
+
+
+
+
+const video = document.querySelector('video');
+video.addEventListener('canplay' , function(){
+    console.log("canplay");
+    
+});
+
+video.addEventListener('play' , function(){
+    console.log("play");
+});
+
+video.addEventListener('pause' , function(){
+    console.log("pause");
+});
+video.addEventListener('playing' , function(){
+    console.log("playing");
+});
+
+video.addEventListener('ended' , function(){
+    console.log("Thanks For watching");
+});
+
+video.addEventListener('volumechange' , function(){
+    console.log("volumechange");
+    
+});
