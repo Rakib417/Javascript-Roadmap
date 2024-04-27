648200a39771270b91903e16ddd4ad031b2e31d4
unknown <rakib417@gmail.com>
2024-04-28 00:39:29 +0600

    Browser Object Model

--- Les 2 - First Js Project/EventBrowserObjectModel.js
+++ Les 2 - First Js Project/EventBrowserObjectModel.js
@@ -3,6 +3,46 @@
 // location object 
 console.clear();
 
+// // href
+// console.log(location.href);
 
-console.log(window.location);
 
+// // protocol
+
+// console.log(location.protocol);
+
+// // Hostname
+// console.log(location.hostname);
+
+// // port 
+// console.log(location.port);
+
+// // pathname 
+// console.log(location.pathname);
+
+
+
+
+const locationdv = document.querySelector(".locattion_div");
+
+const p1 = locationdv.children[0];
+p1.textContent = location.href;
+
+const p2 = locationdv.children[1];
+p2.textContent = location.hostname;
+
+const p3 = locationdv.children[2];
+p3.textContent = location.protocol;
+
+const p4 = locationdv.children[3];
+p4.textContent = location.port;
+
+const p5 = locationdv.children[4];
+p5.textContent = location.pathname;
+
+
+
+const visitbtn = document.getElementById('visits');
+visitbtn.addEventListener('click', function(enet) {
+    location.assign("https://morphlink.com")
+})
