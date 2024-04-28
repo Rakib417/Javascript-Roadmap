ff66675eb01fc691dd80f7e2b6421c8f3c3e1d39
unknown <rakib417@gmail.com>
2024-04-29 00:37:24 +0600

    Browser Object Time Event

--- Les 2 - First Js Project/EventBrowserObjectModelTimeevent.js
+++ Les 2 - First Js Project/EventBrowserObjectModelTimeevent.js
@@ -1,3 +1,52 @@
 // js bom
 // js timing event methods 
 // setTimeout() , setInterval()
+
+
+
+// setTimeout(() => {
+//     console.log("Hi");
+// }, 2000);
+
+
+
+// setTimeout(display, 2000);
+
+// function display() {
+//     console.log("display Function")
+// }
+
+
+
+// const saveBtn = document.querySelector(".savebtn");
+// const msg = document.querySelector(".msg");
+
+// saveBtn.addEventListener("click", saveUser);
+
+// function saveUser() {
+//     msg.textContent = "user Reg Successful";
+//     setTimeout(() => {
+//         msg.textContent = ""
+//     }, 3000);
+
+// }
+
+
+
+
+
+const saveBtn = document.querySelector(".savebtn");
+const msg = document.querySelector(".msg");
+
+saveBtn.addEventListener("click", displaycount);
+
+function displaycount() {
+
+    let count = 1;
+    msg.textContent = count;
+
+    setInterval(() => {
+        count++;
+        msg.textContent = count;
+    }, 2000);
+};
