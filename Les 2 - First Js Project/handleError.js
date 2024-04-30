c0773a11af8e8e1fd0dc9af28d6a79df5f0bd609
rakib417 <rakib417@gmail.com>
2024-04-30 17:59:38 +0600

    Error Handler

---
+++ Les 2 - First Js Project/handleError.js
@@ -0,0 +1,28 @@
+// error handing - > try , catch, finally, throw
+// try ... catch handle run time errors(Program which is)
+// Error Object
+// Rhe finally statement - > execute code, after try and
+
+
+
+
+
+// alert(":hi");
+// alert(x);
+// alert(":By");
+
+
+
+
+try {
+    // code test 
+    alert("hi");
+    alert(x);
+
+} catch (err) {
+    // error handle 
+    console.log(err);
+
+} finally {
+    alert("Bye");
+}
