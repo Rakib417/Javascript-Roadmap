bc68106b26b9b5b7085a5b4487dedd76f78353ae
rakib417 <rakib417@gmail.com>
2024-04-30 15:48:52 +0600

    Time event make a clock

---
+++ Les 2 - First Js Project/bestprac.js
@@ -0,0 +1,21 @@
+// Best Practices 
+// Use Camelcase for Naming 
+// Variable declaration on top and initialize
+// avoid unnecessary variables
+// switch, case, defaultobject and array with const
+// == vs ===
+// access dom less
+console.log();
+
+
+
+const ol = document.querySelector(".stucent-list");
+const firstol = ol.children[0];
+console.log(firstol);
+
+
+
+document.querySelector("button").addEventListener("click", () => {
+    console.log("clicked")
+});
+console.log(submitbtn);
--- Les 2 - First Js Project/bestpractices.js
+++
@@ -1,7 +0,0 @@
-// Best Practices 
-// Use Camelcase for Naming 
-// Variable declaration on top and initialize
-// avoid unnecessary variables
-// switch, case, defaultobject and array with const
-// == vs ===
-// access dom less
