00cecf144c4d01e27410e956ce980ecd74dad0e9
unknown <rakib417@gmail.com>
2024-05-04 02:24:39 +0600

    Basic Js complete now run on little basic advance

--- Les 2 - First Js Project/handleError2.js
+++ Les 2 - First Js Project/handleError2.js
@@ -1,4 +1,22 @@
-const namess = document.querySelector("#ckbtn").addEventListener("click", function() {
-    const nmb = document.querySelector("#nametext").value;
-    console.log(nmb);
+// const namess = document.querySelector("#ckbtn").addEventListener("click", function() {
+//     const nmb = document.querySelector("#nametext").value;
+//     console.log(nmb);
+// });
+
+
+
+const namess = document.querySelector("#ckbtn").addEventListener('click', function() {
+    const nbm = document.querySelector("#nametext").value;
+
+    try {
+        if (nbm < 5) {
+            throw "Input is too low"
+        } else if (nbm > 10) {
+            throw "Input is too High"
+        } else {
+            throw "Input Number Is Perfect"
+        }
+    } catch (err) {
+        console.log(err);
+    }
 });
