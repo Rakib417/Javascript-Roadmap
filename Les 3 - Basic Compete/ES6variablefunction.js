6381f3da786409ee085a443d4538109fee905d1a
rakib417 <rakib417@gmail.com>
2024-05-05 18:15:52 +0600

    ES6 info

--- Les 3 - Basic Compete/ES6variablefunction.js
+++ Les 3 - Basic Compete/ES6variablefunction.js
@@ -6,7 +6,30 @@
 // console.log(x);
 
 
-const x = 11;
-if (true) {
-    x = 10;
+// const x = 11;
+// if (true) {
+//     x = 10;
+// }
+
+
+// let x = 25;
+// let y = 30;
+// console.log(`Sum is ${x + y} .the End`);
+
+
+// let name = "Rakib Ullah";
+// let msg = `I am ${name}. I am learning JS In shaa allah `;
+// console.log(msg);
+
+// // Old
+// function add(x, y) {
+//     var sum = x + y;
+//     console.log(sum);
+// }
+
+const add = (x, y) => {
+    let sum = x + y;
+    console.log(sum);
 }
+
+add(20, 30)
