39658c4c10b30f0ceb3600f4b45876dac405930f
unknown <rakib417@gmail.com>
2024-05-06 00:10:05 +0600

    Rest parameter

--- Les 3 - Basic Compete/hoistingstrictmode.js
+++ Les 3 - Basic Compete/hoistingstrictmode.js
@@ -1,3 +1,22 @@
 "use strict"
-x = 20;
-console.log(`X = ${x}`)
+
+// template 
+// x = 20;
+// console.log(`X = ${x}`)
+
+// Default 
+// function msg(text = "Hello THis is deunt Parameter") {
+//     console.log(`${text}`)
+// }
+
+
+// Srict mode 
+// msg("I Love Js es6");
+// msg();
+
+
+// rest parameter 
+// function Printmethord(x, y, ...z) {
+//     console.log(`X = ${x}, Y = ${y} , Z = ${z}`);
+// }
+// Printmethord(10, 20, 30, 40, 50);
