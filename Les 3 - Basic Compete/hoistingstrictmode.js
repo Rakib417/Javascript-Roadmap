18cadf858e523931cd0b60cb9cc30c88be1c947c
rakib417 <rakib417@gmail.com>
2024-05-06 15:04:33 +0600

    ES6 info

--- Les 3 - Basic Compete/hoistingstrictmode.js
+++ Les 3 - Basic Compete/hoistingstrictmode.js
@@ -1,22 +1,11 @@
-"use strict"
-
-// template 
-// x = 20;
-// console.log(`X = ${x}`)
-
-// Default 
-// function msg(text = "Hello THis is deunt Parameter") {
-//     console.log(`${text}`)
-// }
-
-
-// Srict mode 
-// msg("I Love Js es6");
-// msg();
-
-
-// rest parameter 
-// function Printmethord(x, y, ...z) {
-//     console.log(`X = ${x}, Y = ${y} , Z = ${z}`);
-// }
-// Printmethord(10, 20, 30, 40, 50);
+/*ES6 Features 
+    1. ES6 Syntax
+        1.1 ES6 Variables -> var, let, const(Scope, redeclaration, value assigning)
+        1.2 template literals,
+        1.3 hoisting & strict mode 
+        1.4 Default and reset parameter 
+        1.5 spread operator 
+        1.6 for ... of 
+    2. Array functions 
+    3. Destructuring 
+        3.1 Array and object Destructuring */
