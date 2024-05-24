f497f506c8894e467269735f04159dd87167ddc8
Md. Rakib Ullah <rakib417@gmail.com>
2024-05-25 02:29:43 +0600

    apply demo slider

--- Les 5 - mini project/slider-demo/script.js
+++ Les 5 - mini project/slider-demo/script.js
@@ -1,1 +1,22 @@
 console.clear();
+
+const slideshowElements = document.querySelectorAll(".slideswho-element");
+
+
+let countElements = 1;
+setInterval(() => {
+    countElements++;
+    let currentElement = document.querySelector(".current");
+
+    currentElement.classList.remove("current");
+
+    if(countElements > slideshowElements.length){
+        slideshowElements[0].classList.add("current");
+        countElements = 1;
+    }else{
+        currentElement.nextElementSibling.classList.add("current")
+    }
+
+    
+}, 1000);
+console.log(slideshowElements);
