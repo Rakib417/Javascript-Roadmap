656b843df55d06a1f4a3ab03446fd351941f6746
Md. Rakib Ullah <rakib417@gmail.com>
2024-05-27 19:01:34 +0600

    Slideshow add

--- Les 5 - mini project/slider-demo/HW/HW/main.js
+++ Les 5 - mini project/slider-demo/HW/HW/main.js
@@ -1,1 +1,28 @@
 console.clear();
+
+
+const sliderElements = document.querySelectorAll(".sliderinner");
+let countelement = 1;
+
+
+setInterval(() => {
+    countelement++;
+    const currentElements = document.querySelector(".current");
+    currentElements.classList.remove("current");
+
+    if (countelement> sliderElements.length) {
+        sliderElements[0].classList.add("current");
+        countelement = 1;
+    } else {
+        currentElements.nextElementSibling.classList.add("current");
+        
+    }
+
+
+
+}, 2000);
+
+
+
+
+console.log(sliderElements);
