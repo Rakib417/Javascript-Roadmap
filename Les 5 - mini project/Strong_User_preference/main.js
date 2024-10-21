9639b2687e8241e7c33e776fd7e88566d39a2a49
rakib417 <rakib417@gmail.com>
2024-10-21 11:13:03 +0600

    Userdata local store

--- Les 5 - mini project/Strong_User_preference/main.js
+++ Les 5 - mini project/Strong_User_preference/main.js
@@ -6,15 +6,59 @@ const resetBtn = document.getElementById("resetBtn");
 
 const mainElement = document.querySelector("main");
 
-// changeFontSize 
+const setValues = (fontsize, bgcolor) => {
+    selectFontSize.value = fontsize;
+    selectBgColor.value = bgcolor;
+    mainElement.style.backgroundColor = bgcolor;
+    mainElement.style.fontSize = fontsize;
+}
+
+
+// loadLocalStorage Value
+const initialSetup = () => {
+        const bgcolor = localStorage.getItem("bgcolor");
+        const fontsize = localStorage.getItem("fontsize");
+
+        if (bgcolor && fontsize) {
+            setValues(fontsize, bgcolor)
+
+        }
+        if (!bgcolor && !fontsize) {
+            setValues("16px", "aqua");
+        }
+        if (bgcolor && !fontsize) {
+            setValues("16px", bgcolor);
+        }
+        if (!bgcolor && fontsize) {
+            setValues(fontsize, "aqua");
+        }
+    }
+    // changeFontSize 
 const changeFontSize = (event) => {
-    mainElement.style.fontSize = event.target.value;
+    const selectedFontSize = event.target.value;
+    mainElement.style.fontSize = selectedFontSize;
+    localStorage.setItem("fontsize", selectedFontSize);
 };
+
+
+
 // changeBgColor 
 const changeBgColor = (event) => {
-    mainElement.style.backgroundColor = event.target.value;
+    const selectedBgColor = event.target.value;
+    mainElement.style.backgroundColor = selectedBgColor;
+    localStorage.setItem("bgcolor", selectedBgColor);
+};
+// clearLocalStorage 
+const clearLocalStorage = () => {
+    localStorage.removeItem("bgcolor");
+    localStorage.removeItem("fontsize");
+    setValues("16px", "aqua")
 };
 
 // add event listners 
 selectFontSize.addEventListener("change", changeFontSize);
 selectBgColor.addEventListener("change", changeBgColor);
+resetBtn.addEventListener("click", clearLocalStorage);
+
+
+initialSetup();
