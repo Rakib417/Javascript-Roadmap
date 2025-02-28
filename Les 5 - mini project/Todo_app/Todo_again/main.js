5c7b85d561a2ae8ef35d56b567ff95b1191d1672
rakib417 <rakib417@gmail.com>
2025-02-28 22:41:19 +0600

    Update

--- Les 5 - mini project/Todo_app/Todo_again/main.js
+++
@@ -1,4 +0,0 @@
-const container = document.querySelector(".container")
-const toDoForm = document.querySelector(".todo-form")
-const todoInput = document.querySelector("#inputTodo")
-const todoAddButton = document.querySelector("#addTodobutton")
